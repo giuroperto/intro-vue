5a13397a6090c4c61faaaadef9bb8a12b15b14c0
Giu Roperto <giu.roperto@gmail.com>
2021-01-28 19:09:40 -0300

    lesson 6 done - class and style binding

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ var app = new Vue({
     description: 'These boots are made for walking, and walking is what they do... one of these days these boots are gonna walk all over you.',
     image: './assets/vmSocks-green-onWhite.jpg',
     link: 'https://nocoldfeet.co/collections/green-socks',
-    inStock: true,
-    inventory: 100,
+    inStock: false,
+    inventory: 5,
     onSale: true,
     details: ["80% cotton", "20% polyester", "Gender-neutral"],
     variants: [
