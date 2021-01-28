c98b8c870f9860c6c7b7ee5db5d08210375529dc
Giu Roperto <giu.roperto@gmail.com>
2021-01-28 01:32:17 -0300

    lesson 3 done

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ var app = new Vue({
     product: 'Boots',
     description: 'These boots are made for walking, and walking is what they do... one of these days these boots are gonna walk all over you.',
     image: './assets/vmSocks-green-onWhite.jpg',
-    link: 'https://nocoldfeet.co/collections/green-socks'
+    link: 'https://nocoldfeet.co/collections/green-socks',
+    inStock: true,
+    inventory: 100,
+    onSale: true,
   }
 });
