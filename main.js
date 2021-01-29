b0ddefd9cbc6f3ac8e44474de8edff77653199e6
Giu Roperto <giu.roperto@gmail.com>
2021-01-29 01:15:40 -0300

    lesson 8 done - components

--- main.js
+++ main.js
@@ -1,16 +1,72 @@
-var app = new Vue({
-  el: '#app',
-  data: {
-    brand: 'Vue Mastery',
-    product: 'Boots',
-    description: 'These boots are made for walking, and walking is what they do... one of these days these boots are gonna walk all over you.',
-    selectedVariant: 0,
-    link: 'https://nocoldfeet.co/collections/green-socks',
-    inventory: 5,
-    // onSale: true,
-    details: ["80% cotton", "20% polyester", "Gender-neutral"],
-    variants: [
-      {
+Vue.component('product', {
+  props: {
+    premium: {
+      type: Boolean,
+      required: true,
+    }
+  },
+  template: `
+  <div class="product">
+    <div class="product-image">
+      <a :href="link" target="_blank">
+        <img :src="image" alt="">
+      </a>
+    </div>
+
+    <div class="product-info">
+    <h1>{{ title }}</h1>
+    <p>{{ description }}</p>
+    <p>{{ onSale }}</p>
+    <!-- <p v-show="inStock">In Stock</p> -->
+    <!-- <p v-if="onSale">ON SALE!</p> -->
+    <p v-if="inStock">In Stock</p>
+    <p v-else>Out of Stock</p>
+    <product-details :details="details"></product-details>
+
+    <p>Shipping: {{ shipping }}</p>
+
+    <div v-for="(variant, idx) in variants" :key="variant.variantId"
+    class="color-box"
+    :style="{ backgroundColor: variant.variantColour} "
+    @mouseover="updateProduct(idx)">
+    </div>
+    
+    <div> SIZES:
+      <ul>
+        <li v-for="size in sizes">
+          {{ size }}
+        </li>
+      </ul>
+    </div>
+  </div>
+
+    <button v-on:click="addToCart"
+      :disabled="!inStock"
+      :class="{ disabledButton: !inStock}"
+      >Add to Cart</button>
+
+    <button v-on:click="removeFromCart">Remove from Cart</button>
+
+    <div class="cart">
+      <p>Cart({{cart}})</p>
+    </div>
+    <!-- <p v-if="inventory > 10">In Stock</p>
+    <p v-else-if="inventory <= 10 && inventory > 0"> Almost Sold Out!</p>
+    <p v-else
+      :class="{classLineThrough: !inStock}">Out of Stock</p> -->
+  </div>
+  `,
+  data() {
+    return {
+      brand: 'Vue Mastery',
+      product: 'Boots',
+      description: 'These boots are made for walking, and walking is what they do... one of these days these boots are gonna walk all over you.',
+      selectedVariant: 0,
+      link: 'https://nocoldfeet.co/collections/green-socks',
+      inventory: 5,
+      // onSale: true,
+      details: ["80% cotton", "20% polyester", "Gender-neutral"],
+      variants: [{
         variantId: 2234,
         variantColour: "green",
         variantImage: "./assets/vmSocks-green-onWhite.jpg",
@@ -22,10 +78,10 @@ var app = new Vue({
         variantImage: "./assets/vmSocks-blue-onWhite.jpg",
         variantQuantity: 0,
         onSale: false,
-      }
-    ],
-    sizes: ["P", "M", "G"],
-    cart: 0,
+      }],
+      sizes: ["P", "M", "G"],
+      cart: 0,
+    };
   },
   methods: {
     addToCart: function () {
@@ -37,7 +93,7 @@ var app = new Vue({
       // console.log(index);
     },
     removeFromCart() {
-      if(this.cart >= 1) {
+      if (this.cart >= 1) {
         this.cart -= 1;
       }
     }
@@ -54,6 +110,33 @@ var app = new Vue({
     },
     onSale() {
       return this.variants[this.selectedVariant].onSale ? this.brand + ' ' + this.product + ' is on sale' : this.brand + ' ' + this.product + ' is not on sale';
+    },
+    shipping() {
+      if (this.premium) {
+        return "Free";
+      }
+      return 2.99;
     }
+  }
+});
+
+Vue.component('product-details', {
+  props: {
+    details: {
+      type: Array,
+      required: true,
+    }
+  },
+  template: `
+    <ul>
+      <li v-for="detail in details"> {{detail}} </li>
+    </ul>
+  `
+});
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    premium: true,
   }
 });
